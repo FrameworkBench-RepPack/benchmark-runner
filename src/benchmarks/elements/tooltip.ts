7f40fe82ca578152e320a89fe9c654bdc5445c23
Anton <dev@atjn.dk>
2025-11-23 18:17:46 +0100

    Validate tooltip behavior during tests

--- src/benchmarks/elements/tooltip.ts
+++ src/benchmarks/elements/tooltip.ts
@@ -1,15 +1,38 @@
-import { WebElement } from "selenium-webdriver";
+import { By, WebElement } from "selenium-webdriver";
 import { Driver } from "selenium-webdriver/firefox";
 import {
   promisifiedTimeout,
   simulateClick,
 } from "../../utilities/benchmark-utilities";
 
+const contentSelector = ".tooltip + .contents";
+
+/**
+ * Waits until the specified number of tooltips are open.
+ *
+ * @param driver The driver to control the browser instance.
+ * @param amount How many tooltips should be open.
+ */
+async function tooltipsOpen(driver: Driver, amount: number) {
+  return await driver.wait(async () => {
+    const elements = await driver.findElements(By.css(contentSelector));
+    let openTooltips = 0;
+    for (const el of elements) {
+      if (await el.isDisplayed()) {
+        openTooltips++;
+      }
+    }
+    return openTooltips === amount;
+  }, 5000);
+}
+
 export async function testTooltip(driver: Driver, tooltipElement: WebElement) {
   await simulateClick(driver, tooltipElement);
 
-  await promisifiedTimeout(500);
+  await tooltipsOpen(driver, 1);
+  await promisifiedTimeout(200);
 
-  // Click away the tooltip
+  // Dismiss the tooltip
   await driver.actions({ async: true }).move({ x: 0, y: 0 }).click().perform();
+  await tooltipsOpen(driver, 0);
 }
