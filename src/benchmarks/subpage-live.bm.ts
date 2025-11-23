235c5b0cf1afb615f5fa3a67aa77969744d5d8da
Anton <dev@atjn.dk>
2025-11-23 19:15:39 +0100

    Validate live data during tests

--- src/benchmarks/subpage-live.bm.ts
+++ src/benchmarks/subpage-live.bm.ts
@@ -1,3 +1,4 @@
+import { By } from "selenium-webdriver";
 import { Driver } from "selenium-webdriver/firefox";
 import {
   loadPage,
@@ -9,6 +10,26 @@ import BenchmarkInput from "./benchmark-types";
 
 const BENCHMARK_NAME = "subpage-live" as const;
 
+const dataSelector = "#live-data td";
+
+/**
+ * Waits until the specified datapoints are shown.
+ *
+ * @param driver The driver to control the browser instance.
+ * @param data The data that should be shown.
+ */
+async function showsData(driver: Driver, data: [number, number, number]) {
+  return await driver.wait(async () => {
+    const elements = await driver.findElements(By.css(dataSelector));
+    for (let i = 0; i < 3; i++) {
+      if ((await elements[i]?.getText()) !== String(data[i])) {
+        return false;
+      }
+    }
+    return true;
+  }, 4000);
+}
+
 export default async function benchmark(options: BenchmarkInput) {
   const prepareTest = async (driver: Driver) => {
     await prepareBrowser(driver);
@@ -17,8 +38,10 @@ export default async function benchmark(options: BenchmarkInput) {
   const performTest = async (driver: Driver) => {
     await loadPage(driver, options.link + "/live/");
 
-    // Let the page run for 16 seconds, which is the time required to cycle through all data points
-    await promisifiedTimeout(16000);
+    // Let the page run for 14-18 seconds, depending on when the last data point is shown. It should be shown after 16 seconds.
+    await promisifiedTimeout(14000);
+    await showsData(driver, [51, 35, 72]);
+    await promisifiedTimeout(100);
   };
 
   await profilerWrapper({
